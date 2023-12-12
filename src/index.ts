004e59783858fc9e95467cff41b1ea1e578dcf8f
Leonardo <lrlessa@gmail.com>
2023-12-12 15:40:39 -0300

    Fix RhfAutocomplete label inconsistency

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,11 @@ import RhfDatePicker from "./components/RhfDatePicker";
 import RhfSwitch from "./components/RhfSwitch";
 import RhfTextField from "./components/RhfTextField";
 
+// export all types from components
+export * from "./components/RhfAutocomplete/RhfAutocomplete.types";
+export * from "./components/RhfCheckbox/RhfCheckbox.types";
+export * from "./components/RhfDatePicker/RhfDatePicker.types";
+export * from "./components/RhfSwitch/RhfSwitch.types";
+export * from "./components/RhfTextField/RhfTextField.types";
+
 export { RhfAutocomplete, RhfCheckbox, RhfDatePicker, RhfSwitch, RhfTextField };
