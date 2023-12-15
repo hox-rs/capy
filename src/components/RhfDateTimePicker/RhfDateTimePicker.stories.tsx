7abebdaa43bf3c76e7682b927e7ded558b11e635
Davi Pavone <davipavone@gmail.com>
2023-12-15 15:57:51 -0300

    feat: add RhfDateTimePicker component

---
+++ src/components/RhfDateTimePicker/RhfDateTimePicker.stories.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import RhfDateTimePicker from "./RhfDateTimePicker";
+import { useForm } from "react-hook-form";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
+const meta: Meta<typeof RhfDateTimePicker> = {
+  component: RhfDateTimePicker,
+  title: "HOX Capy/RhfDateTimePicker",
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof RhfDateTimePicker>;
+
+export const Standard: Story = (args) => {
+  const { control } = useForm();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RhfDateTimePicker control={control} name={args.name} {...args} />
+    </LocalizationProvider>
+  );
+};
+
+Standard.args = {
+  disabled: false,
+  name: "name",
+  label: "Field label",
+};
+
+export const Disabled: Story = (args) => {
+  const { control } = useForm();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RhfDateTimePicker control={control} name={args.name} {...args} />
+    </LocalizationProvider>
+  );
+};
+
+Disabled.args = {
+  disabled: true,
+  name: "name",
+  label: "Field label",
+};
+
+export const Error: Story = (args) => {
+  const { control } = useForm();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RhfDateTimePicker control={control} name={args.name} {...args} />
+    </LocalizationProvider>
+  );
+};
+
+Error.args = {
+  disabled: false,
+  name: "name",
+  label: "Field label",
+  error: { message: "Error message", type: "error" },
+};
+
+export const WithDefaultValue: Story = (args) => {
+  const { control } = useForm();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RhfDateTimePicker control={control} name={args.name} {...args} />
+    </LocalizationProvider>
+  );
+};
+
+WithDefaultValue.args = {
+  disabled: false,
+  name: "name",
+  label: "Field label",
+  defaultValue: new Date("2021-09-01 04:00:00"),
+};
+
+export const WithHelperText: Story = (args) => {
+  const { control } = useForm();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RhfDateTimePicker control={control} name={args.name} {...args} />
+    </LocalizationProvider>
+  );
+};
+
+WithHelperText.args = {
+  disabled: false,
+  name: "name",
+  label: "Field label",
+  helperText: "Helper text",
+};
---
+++ src/components/RhfDateTimePicker/RhfDateTimePicker.test.tsx
@@ -0,0 +1,113 @@
+// this component is a wrapper of Material-ui DatePicker that implements useController by react-hook-form
+// a resumed version of the component implementation is:
+//
+// <RhfDateTimePicker
+//   control={control}
+//   name="field-name" // used in validations and useForm
+//   label="Field label" // label of the field
+//   defaultValue={new Date()} // default value of the field
+//   error={error} // error object from react-hook-form
+//   {...props} // rest of the props from mui DateTimePicker
+// />;
+// these are brief tests just to check if the union between these two libs are working
+// as they have their own tests, we don't need to test all the cases
+
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
+import RhfDateTimePicker from "./RhfDateTimePicker";
+
+import { useForm } from "react-hook-form";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+
+// create element that implements control and wraps because hooks can only be used inside a component
+const RhfDateTimePickerWrapper = (props: any) => {
+  const { control } = useForm();
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <RhfDateTimePicker control={control} {...props} />;
+    </LocalizationProvider>
+  );
+};
+
+describe("Testing <RhfDateTimePicker />", () => {
+  it("should render RhfDateTimePicker with few props", () => {
+    const { getByRole } = render(
+      <RhfDateTimePickerWrapper
+        name="field-name"
+        label="Field label"
+        defaultValue={new Date()}
+      />
+    );
+
+    expect(getByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("should render RhfDateTimePicker with a default value", () => {
+    const { getByRole } = render(
+      <RhfDateTimePickerWrapper
+        name="field-name"
+        label="Field label"
+        defaultValue={new Date("2021-09-01 04:00:00")}
+      />
+    );
+
+    expect(getByRole("textbox")).toHaveValue("09/01/2021 04:00 AM");
+  });
+
+  it("should render RhfDateTimePicker with an error", () => {
+    const { getByText } = render(
+      <RhfDateTimePickerWrapper
+        name="field-name"
+        label="Field label"
+        defaultValue={new Date()}
+        error={{ message: "error message" }}
+      />
+    );
+
+    expect(getByText("error message")).toBeInTheDocument();
+  });
+
+  it("should render RhfDateTimePicker with a helper text", () => {
+    const { getByText } = render(
+      <RhfDateTimePickerWrapper
+        name="field-name"
+        label="Field label"
+        defaultValue={new Date()}
+        helperText="helper text"
+      />
+    );
+
+    expect(getByText("helper text")).toBeInTheDocument();
+  });
+
+  it("should render RhfDateTimePicker with a variant", () => {
+    const { getByRole } = render(
+      <RhfDateTimePickerWrapper
+        name="field-name"
+        label="Field label"
+        defaultValue={new Date()}
+        variant="standard"
+      />
+    );
+
+    expect(getByRole("textbox")).toHaveClass("MuiInputBase-input");
+  });
+
+  it("should render RhfDateTimePicker with fullWidth false ", () => {
+    const { getByRole } = render(
+      <RhfDateTimePickerWrapper
+        name="field-name"
+        label="Field label"
+        defaultValue={new Date()}
+        fullWidth={false}
+      />
+    );
+
+    expect(getByRole("textbox")).not.toHaveClass("MuiInputBase-fullWidth");
+  });
+});
