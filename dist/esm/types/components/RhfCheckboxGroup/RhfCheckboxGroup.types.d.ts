75728a421f2e402b9e4c7116e015936a31b3c194
Leonardo <lrlessa@gmail.com>
2023-12-22 15:35:04 -0300

    Add RhfCheckboxGroup and RhfRadioGroup components

---
+++ dist/esm/types/components/RhfCheckboxGroup/RhfCheckboxGroup.types.d.ts
@@ -0,0 +1,13 @@
+import { FieldError, FieldPath, FieldValues, Merge, UseControllerProps } from "react-hook-form";
+export type RhfCheckboxGroupOption = {
+    label: string;
+    value: string;
+    disabled?: boolean;
+};
+export type RhfCheckboxGroupProps<TFieldValues extends FieldValues, TName extends FieldPath<TFieldValues>> = UseControllerProps<TFieldValues, TName> & {
+    error?: Merge<FieldError, (FieldError | undefined)[]> | undefined;
+    label: string;
+    options: RhfCheckboxGroupOption[];
+    disabled?: boolean;
+    row?: boolean;
+};
